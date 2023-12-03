726458dec6ec564341fc15dbd47bce7339a4f0f9
Rafael Gebim <rafaelgebim@gmail.com>
2023-12-03 16:11:42 -0300

    primeira execução do servidor

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app = express()
+
+app.get('/', (requisicao, resposta)=>{
+    resposta.send("olá, mundo")
+})
+
+app.listen(3000, ()=> {
+    console.log("servidor rodando na porta 3000!")
+})
