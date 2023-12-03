1a9b962b73f22967c7e961fb73befc7899d2a729
Rafael Gebim <rafaelgebim@gmail.com>
2023-12-03 17:46:33 -0300

    Adição dos icones de lua e de sol para troca de tema

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express()
 app.engine('handlebars', exphbs.engine())
 app.set('view engine','handlebars')
 
+
+app.use(express.static('public'))
+
 app.get('/', (requisicao, resposta)=>{
     resposta.render('home')
 })
